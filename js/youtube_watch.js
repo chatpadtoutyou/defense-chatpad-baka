53048d5a214c0d758416d7fba4eaf43f37c1572c
chatpadtoutyou <chatpadtoutyou@gmail.com>
2023-07-13 23:59:54 +0900

    順番修正

--- js/youtube_watch.js
+++ js/youtube_watch.js
@@ -10,7 +10,7 @@ setTimeout(() => {
 }, settingsButtonTime + 400);
 
 setTimeout(() => {
-    $('.ytp-menuitem:first-child').click();
+    $('.ytp-menuitem:nth-child(1)').click();
 }, settingsButtonTime + 1000);
 
 const showNames = [];
